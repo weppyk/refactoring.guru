d78ad37b165ec99978d835faa8859ad986ff088c
Radek Drlík <radek.drlik@gmail.com>
2022-10-05 15:00:02 +0200

    Inheritance and Implementation

--- UML/CompareAllRelations.php
+++ UML/CompareAllRelations.php
@@ -1,5 +1,6 @@
 <?php
-/** ASSOCIATION - is generalized concept of relations
+/** ASSOCIATION  
+ * |Person| -------------> |Book|
  * (has-a)
  * 
  * Person has relationship with Book
@@ -14,16 +15,17 @@ class PersonAssociation
         $this->book = $book;
     }
 }
-/** COMPOSITION  
- * (has-a + whole-part + ownership)
+/** COMPOSITION
+ * |Person| -------------<#> |Book|
+ * (has-a  + whole-part + ownership)
  * 
  * Person own $book and multimedia for whole life,
  * when person dies book and multimedia is also destroyed
  */
 class PersonComposition
 {
-    private $book;
-    private $multimedia = [];
+    private $book; //part-of
+    private $multimedia = []; //whole-part
     public function __construct()
     {
         $this->book = new Book;
@@ -35,7 +37,8 @@ class PersonComposition
 }
 
 /** AGGREGATION
- * (has-a + whole-part) single or collection
+ * |Person| -------------<> |Book|
+ * (has-an + whole-part)
  * 
  * book, movie and multimedia is borrowed from someone else
  * when person dies, book and movie is not destroyed
@@ -67,7 +70,8 @@ $person->addMultimedia(new Book());
 $person->addMultimedia(new Movie());
 
 
-/** DEPENDECY
+/** DEPENDECY 
+ * |Person| - - - - - -> |Book|
  * 
  * there is no conceptual link between two objects
  * person does not own book, just read it
@@ -79,4 +83,43 @@ class PersonDependency
     }
 }
 
+/** INHERITANCE / GENERALIZATION
+ * |Person| -------------|> |Student|
+ * (is-a)
+ * 
+ * Student is a Person
+ */
+class PersonInheritance
+{
+    private $name;
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+}
+class Student extends PersonInheritance
+{
+    private $studentId;
+    public function __construct($name, $studentId)
+    {
+        parent::__construct($name);
+        $this->studentId = $studentId;
+    }
+}
 
+/** REALIZATION / IMPLEMENTATION
+ * |Person| - - - - - -|> |LibrarianPerson|
+ * 
+ * Person is interface for LibrarianPerson
+ */
+interface Person
+{
+    public function eatFood(Food $food);
+}
+class LibrarianPerson implements Person
+{
+    public function eatFood(Food $food)
+    {
+        return "I am eating ".$food->name;
+    }
+}
